8f0b65ffc3ba9925f9d3f29f3dc0041ee5d2b45f
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-06-23 00:00:59 +0530

    quiz-app

---
+++ script.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const startBtn=document.getElementById("start-btn")
+    const nextBtn=document.getElementById("next-btn")
+    const restartBtn=document.getElementById("restart-btn")
+    const questionContainer=document.getElementById('question-container')
+    const choicesList=document.getElementById('choices-list')
+    const resultContainer=document.getElementById('result-container')
+    const scoreDisplay=document.getElementById('score')
+    const questionText=document.getElementById('question-text')
+
+    const questions=[
+        {
+            question:"What is the capital of France?",
+            choices:["Paris","London","Berlin","Madrid"],
+            answer:"Paris"
+        },
+        {
+            question:"Which planet is known as the Red Planet?",
+            choices:["Mars","Venus","Jupitar","Saturn"],
+            answer:"Mars"
+        }
+        
+    ]
+
+  let currentQuestionIndex=0;
+  let score=0;
+  startBtn.addEventListener("click",startQuiz)
+  function startQuiz(){
+     startBtn.classList.add('hidden')
+    console.log('hi')
+    resultContainer.classList.add('hidden')
+    questionContainer.classList.remove('hidden')
+    showQuestion()
+  }
+
+  nextBtn.addEventListener('click',()=>{
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+      showQuestion()
+    }
+    else{
+      showResult()
+    }
+  })
+
+  restartBtn.addEventListener('click',()=>{
+currentQuestionIndex=0;
+score=0;
+resultContainer.classList.add('hidden')
+startQuiz()
+  })
+
+  function showQuestion(){
+    nextBtn.classList.add('hidden')
+    questionText.textContent=questions[currentQuestionIndex].question
+    choicesList.innerHTML=""//clear previous question
+    questions[currentQuestionIndex].choices.forEach(choice=>{
+      const li=document.createElement('li')
+      
+      li.textContent=choice
+      //selectAnswer function requires a function but we can't pass
+      //parameter in addEventListener
+      li.addEventListener('click',()=>selectAnswer(choice))
+      choicesList.appendChild(li)
+    })
+  }
+  function selectAnswer(choice){
+       const correctAnswer=questions[currentQuestionIndex].answer
+       if(choice===correctAnswer){
+        score++
+       }
+       nextBtn.classList.remove("hidden")
+  }
+
+  function showResult(){
+     questionContainer.classList.add('hidden')
+     resultContainer.classList.remove('hidden')
+     scoreDisplay.textContent=`${score} out of ${questions.length}`
+  }
+
+})
